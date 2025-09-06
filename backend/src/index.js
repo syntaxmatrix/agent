e1c5edbfe5fd07db2b5c7a93697021b2294585c8
Rahul Singh <rahul274301@gmail.com>
2025-09-06 12:35:45 +0530

    chore: update dependencies and add dotenv and nodemon
    - Added dotenv as a dependency (version 17.2.2).
    - Added nodemon as a devDependency (version 3.1.10).
    - Updated package-lock.json to reflect changes in dependencies and their versions.
    - Included additional dependencies for nodemon and other related packages.

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env.backend",
+});
+
+const app = express()
+
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is RUNNING at PORT :", process.env.PORT);
+});
+
+
+app.get("/", (req, res) => {
+  res.send("Backend API running 🚀");
+});
