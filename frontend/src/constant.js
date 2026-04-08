bf09596ccb8da7ffd7bee1b3d38030c1b0fbca3c
Rahul Singh <rahul274301@gmail.com>
2026-04-08 22:28:48 +0530

    feat: update environment variables for product name and version in configuration

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -2,6 +2,6 @@ export const BACKEND_GOOGLE_URL = "http://localhost:8000/api/v1/user/google";
 
 export const BACKEND_GOOGLE_GMAIL_URL = "http://localhost:8000/api/v1/user/gmail";
 
-export const ProductName = "Agentic AI";
+export const ProductName = process.env.PRODUCT_NAME;
 
-export const Version = "v0.2";
+export const Version = process.env.VERSION;
